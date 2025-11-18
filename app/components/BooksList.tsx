8dbf62b0d5c5c9119deece3ca185d827975004ca
Carlos Alejandro <27aniconsol@gmail.com>
2025-11-17 22:44:13 -0500

    Cambiando estilos

--- app/components/BooksList.tsx
+++ app/components/BooksList.tsx
@@ -1,25 +1,23 @@
 "use client";
 
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
-
-interface Author{
-    name:string;
+interface Author {
+  name: string;
 }
 
 interface Book {
-    id:number;
-    title:string;
-    authors:Author[];
+  id: number;
+  title: string;
+  authors: Author[];
 }
 
-
 export default function BooksList() {
-    const [books,setBooks] = useState<Book[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
+  const [books, setBooks] = useState<Book[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
+  useEffect(() => {
     async function fetchBooks() {
       try {
         const response = await fetch("https://gutendex.com/books/?page=1");
@@ -37,21 +35,32 @@ export default function BooksList() {
     fetchBooks();
   }, []);
 
-  if (loading) return <p>Cargando libros...</p>;
-  if (error) return <p>{error}</p>;
-
+  if (loading) return <div>Cargando libros...</div>;
+  if (error) return <div>Error: {error}</div>;
 
   return (
-     <div>
+    <div style={{ padding: '20px' }}>
       <h2>Lista de Libros</h2>
-      <ul>
-        {books.map((book) => (
-          <li key={book.id}>
-            <strong>{book.title}</strong> —{" "}
-            {book.authors.length > 0 ? book.authors[0].name : "Autor desconocido"}
-          </li>
-        ))}
-      </ul>
+      <table border={1} style={{ width: '100%', borderCollapse: 'collapse' }}>
+        <thead>
+          <tr style={{ backgroundColor: '#0c4475ff' }}>
+            <th style={{ padding: '8px', border: '1px solid #ccc' }}>ID</th>
+            <th style={{ padding: '8px', border: '1px solid #ccc' }}>Título</th>
+            <th style={{ padding: '8px', border: '1px solid #ccc' }}>Autor</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((book) => (
+            <tr key={book.id}>
+              <td style={{ padding: '8px', border: '1px solid #ccc' }}>{book.id}</td>
+              <td style={{ padding: '8px', border: '1px solid #ccc' }}>{book.title}</td>
+              <td style={{ padding: '8px', border: '1px solid #ccc' }}>
+                {book.authors.length > 0 ? book.authors[0].name : "Autor desconocido"}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
